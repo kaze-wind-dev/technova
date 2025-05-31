63f6e5e5370b8c306991e472b7633e6f93c94ef7
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-05-31 21:57:30 +0900

    News詳細ページの追加

---
+++ app/news/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { getNewsDetail } from "@/libs/microcms";
+import LinkButton from "@/components/LinkButton";
+import Detail from "@/components/Detail";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function NewsDetailPage({ params }: Props) {
+  const data = await getNewsDetail(params.id);
+
+  return (
+    <main>
+      <div className="py-16 md:py-20 lg:py-24">
+        <Detail data={data} />
+        <LinkButton
+          href="/news"
+          addClass="bg-primary text-white font-bold hover:bg-primary-hover mx-auto mt-10 md:mt-12 lg:mt-16"
+        >
+          一覧に戻る
+        </LinkButton>
+      </div>
+    </main>
+  );
+}
