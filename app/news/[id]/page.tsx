a7765b6f4aabe6e06caca648ccf639793f25c1c1
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-01 19:51:27 +0900

    NewsDetailをコンポーネントへ分離

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { getNewsDetail } from "@/libs/microcms";
 import LinkButton from "@/components/LinkButton";
-import Detail from "@/components/Detail";
+import NewsDetail from "@/components/NewsDetail";
 
 type Props = {
   params: {
@@ -14,7 +14,7 @@ export default async function NewsDetailPage({ params }: Props) {
   return (
     <main>
       <div className="py-16 md:py-20 lg:py-24">
-        <Detail data={data} />
+        <NewsDetail data={data} />
         <LinkButton
           href="/news"
           addClass="bg-primary text-white font-bold hover:bg-primary-hover mx-auto mt-10 md:mt-12 lg:mt-16"
