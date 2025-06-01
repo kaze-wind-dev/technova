8f93e2ac68c2300c606df19adc55fa9dde749c0f
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-01 19:58:13 +0900

    LinkButtonコンポーネントの修正

--- components/LinkButton/index.tsx
+++ components/LinkButton/index.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 const LinkButton = ({ href, addClass, blank, children }: Props) => {
   const btnClass = addClass
-    ? `block w-fit py-4 px-8 rounded-full transition ${addClass}`
-    : "block w-fit py-4 px-8 rounded-full transition";
+    ? `block w-fit py-[1em] px-[2em] rounded-full transition ${addClass}`
+    : "block w-fit py-[1em] px-[2em] rounded-full transition";
 
   return (
     <a className={btnClass} href={href} target={blank}>
