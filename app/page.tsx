3decd01bd642f119ebf00302582c7e5937dfe6d0
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-01 19:49:55 +0900

    事業内容セクションの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 // import Image from "next/image";
 import { getNewsList } from "@/libs/microcms";
+import { servicesData } from "@/libs/utiles";
 
 import Hero from "@/components/Hero";
 import SectionTitle from "@/components/SectionTitle";
@@ -7,39 +8,58 @@ import LinkButton from "@/components/LinkButton";
 
 import Inner from "@/components/Inner";
 import Section from "@/components/Section";
+import ServiceList from "@/components/ServiceList/page";
 import NewsList from "@/components/NewsList";
 
 export default async function Home() {
-  const {contents:news} = await getNewsList({
-      limit: 5,
-    });
+  const { contents: news } = await getNewsList({
+    limit: 5,
+  });
   return (
     <main>
-      <Hero 
-    pageTitle="未来を見据えたWeb戦略と開発力で、あなたのビジネスを加速させます。"
-    pageDesc="Boost Your Future."
-  pageRoot={true}
-    image={{
-      src: '/mainVisual-img.jpg',
-      alt: "ファーストビュー", 
-      width: 1512, 
-      height: 694, 
-    }
-  }
+      <Hero
+        pageTitle={`未来を見据えたWeb戦略と開発力で、\nあなたのビジネスを加速させます。`}
+        pageDesc="Boost Your Future."
+        pageRoot={true}
+        image={{
+          src: "/mainVisual-img.jpg",
+          alt: "ファーストビュー",
+          width: 1512,
+          height: 694,
+        }}
       />
-    <Section>
+      <Section>
+        <Inner>
+          <SectionTitle
+            mainTitle="事業内容"
+            subTitle="Service"
+            horizontal="center"
+          />
+          <ServiceList services={servicesData}/>
+        </Inner>
+        <LinkButton
+          href="/services"
+          addClass="bg-primary text-white font-bold hover:bg-primary-hover btn-arrow-white mx-auto mt-8 md:mt-10 lg:mt-12"
+        >
+          <span>もっと見る</span>
+        </LinkButton>
+      </Section>
+      <Section>
         <Inner>
           <SectionTitle
             mainTitle="お知らせ"
             subTitle="News"
             horizontal="center"
           />
-          <NewsList news={news}/>
-
+          <NewsList news={news} />
         </Inner>
-      <LinkButton href="/news" addClass="bg-primary text-white font-bold hover:bg-primary-hover btn-arrow-white mx-auto mt-8 mg:10 lg:mt-12"><span>もっと見る</span></LinkButton>
+        <LinkButton
+          href="/news"
+          addClass="bg-primary text-white font-bold hover:bg-primary-hover btn-arrow-white mx-auto mt-8 md:mt-10 lg:mt-12"
+        >
+          <span>もっと見る</span>
+        </LinkButton>
       </Section>
-
     </main>
   );
 }
