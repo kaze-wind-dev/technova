2d70f2c4b2b9bedd57c7b1fe4ec5bdf3fae1ced2
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-01 19:54:27 +0900

    事例紹介ページのページネーションを追加

---
+++ app/works/page/[current]/page.tsx
@@ -0,0 +1,50 @@
+import { notFound } from "next/navigation";
+import { getNewsList } from "@/libs/microcms";
+
+import { NEWS_LIST_LIMIT } from "@/constants";
+
+import Hero from "@/components/Hero";
+import SectionTitle from "@/components/SectionTitle";
+import Inner from "@/components/Inner";
+import Section from "@/components/Section";
+import NewsList from "@/components/NewsList";
+import Pagination from "@/components/Pagination";
+
+type Props = {
+  params: {
+    current: string
+  }
+}
+
+export default async function NewsPageId({params}:Props) {
+  const current = parseInt(params.current as string, 10);
+
+  if (Number.isNaN(current) || current < 1) {
+    notFound();
+  }
+
+  const {contents:news, totalCount} = await getNewsList({
+    limit:  NEWS_LIST_LIMIT,
+    offset:  NEWS_LIST_LIMIT * (current - 1),
+  });
+
+  return (
+    <main>
+      <Hero
+        pageTitle="お知らせ"
+        pageDesc={`TechNovaからのお知らせや最新情報をご案内します。サービス情報、実績公開、採用に関する情報などはこちらからご確認ください。`}
+      />
+      <Section>
+        <Inner>
+          <SectionTitle
+            mainTitle="お知らせ一覧"
+            subTitle="News"
+            horizontal="center"
+          />
+          <NewsList news={news}/>
+          <Pagination totalCount={totalCount} prepage={NEWS_LIST_LIMIT} basePath="news" current={current}/>
+        </Inner>
+      </Section>
+    </main>
+  );
+}
