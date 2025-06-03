2aab14425725128f7480249bbc63eb968bf496fe
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-03 23:42:02 +0900

    CategoryFilterコンポーネントの追加

---
+++ components/CategoryFilter/page.tsx
@@ -0,0 +1,41 @@
+import type { Category } from "@/libs/microcms";
+
+import LinkButton from "../LinkButton";
+
+type Props = {
+  categories: Category[];
+  basePath: string;
+  currentId?: string;
+};
+
+const CategoryFilter = ({
+  categories,
+  basePath = "news",
+  currentId,
+}: Props) => {
+  return (
+    <ul className="flex items-center justify-start gap-3 md:gap:4 lg:gap-5 mx-auto mb-10 md:mb-12 lg:mb-14">
+      {categories.map((category) =>
+        currentId === category.id ? (
+          <span
+            className="block w-fit py-[1em] px-[2em] rounded-full bg-primary text-white font-bold"
+            key={category.id}
+          >
+            {category.name}
+          </span>
+        ) : (
+          <li key={category.id}>
+            <LinkButton
+              addClass="bg-gray-medium text-black font-bold hover:bg-primary hover:text-white"
+              href={`/${basePath}/category/${category.id}`}
+            >
+              {category.name}
+            </LinkButton>
+          </li>
+        )
+      )}
+    </ul>
+  );
+};
+
+export default CategoryFilter;
