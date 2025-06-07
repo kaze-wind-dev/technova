2613678a06ca66f7ebda4f1b047278cfe47c3ac6
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-06-08 00:47:01 +0900

    next.configの編集

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.microcms-assets.io" },
     ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
